c3e67645bc7333d925be2b81eaff1d03770e2b90
paffenst <paffen.st@gmail.com>
2019-02-15 23:06:40 +0100

    add InMemoryDataService

--- my-cinema-app/src/app/app-routing.module.ts
+++ my-cinema-app/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { BuyComponent } from './buy/buy.component';
+import { MovieComponent } from './movie/movie.component';
+import { DayComponent } from './day/day.component';
+import { CinemaComponent } from './cinema/cinema.component';
+import { ScreeningsComponent } from './screenings/screenings.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/allMovies', pathMatch: 'full'},
+  {path: 'allMovies', component: DashboardComponent},
+  {path: 'allScreenings', component: ScreeningsComponent},
+  {path: 'cinema/:id', component: CinemaComponent},
+  {path: 'day/:year/:month/:day', component: DayComponent},
+  {path: 'movie/:title', component: MovieComponent},
+  {path: 'buy/:id', component: BuyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
