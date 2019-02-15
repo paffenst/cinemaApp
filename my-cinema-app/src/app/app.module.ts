c6e14cee8ab1b48c74517c02af5465ec61861251
paffenst <paffen.st@gmail.com>
2019-02-15 17:47:55 +0100

    add all components

--- my-cinema-app/src/app/app.module.ts
+++ my-cinema-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ScreeningsComponent } from './screenings/screenings.component';
+import { CinemaComponent } from './cinema/cinema.component';
+import { DayComponent } from './day/day.component';
+import { MovieComponent } from './movie/movie.component';
+import { BuyComponent } from './buy/buy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ScreeningsComponent,
+    CinemaComponent,
+    DayComponent,
+    MovieComponent,
+    BuyComponent
   ],
   imports: [
     BrowserModule,
